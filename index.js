4dbca57a228ca8c5b6429fd8a4b36b7f68ad4135
LourdesGarcia <lourdesgarciafyl@gmail.com>
2023-06-28 17:05:30 -0300

    agregamos un archivo estatico

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import * as dotenv from "dotenv";
 import morgan from "morgan";
+import path from "path";
 
 dotenv.config() // si o si antes de mencionar una variable de entorno
 
@@ -15,10 +16,17 @@ app.listen(app.get("PORT"), () =>{
     console.log("Estoy en el puerto "+ app.get("PORT"))
 })
 
-//middelwares: funciones que se ejecutan antes de las rutas
+//middlewares: funciones que se ejecutan antes de las rutas
 app.use(express.json()); //permite interpretar el formato json en un request
 app.use(express.urlencoded({extended: true})); // permite interpretar strings y array del request
 app.use(cors()); //permite conexiones remotas
 app.use(morgan("dev")); //da info extra en la terminar
+// ultimo middlewere para CARGAR UN ARCHIVO ESTATICO
+console.log(path.join(__dirname, `/public`));
+app.use(express.static(path.join(__dirname, `/public`)))
 
-// rutas
+
+// rutas: a donde se hara una solicitud
+app.get("/prueba", (req, res) => {
+    res.send("Esta es una prueba de mi ruta get")
+})
