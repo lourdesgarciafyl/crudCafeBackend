6dbded859351dc89ed0da504ba92d43c04d0572b
LourdesGarcia <lourdesgarciafyl@gmail.com>
2023-07-03 16:57:06 -0300

    agregamos al controlador la funcion obtenerListaProductos

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -7,14 +7,27 @@ export const controladorPrueba = (req, res) => {
 export const crearProducto = async (req, res) => {
     try{
         const productoNuevo = new Producto(req.body);
-        await productoNuevo.save();
+        await productoNuevo.save(); // save es una querie o metodo de mongoose para guardar en la coleccion
         res.status(201).json({
             mensaje: "El producto fue creado correctamente"
         })
     }catch(error){
         console.log(error)
         res.status(404).json({
-            mensaje: "Error. No se puso crear el producto"
+            mensaje: "Error. No se pudo crear el producto"
+        })
+    }
+}
+
+export const obtenerListaProductos = async (req, res) =>{
+    try{
+        // buscar en la base de datos la coleccion de productos
+        const productos = await Producto.find(); // find de mongoose es una querie que devuelve una lista de los documentos (osea, de todos los productos)
+        res.status(200).json(productos); // mando al front la lista de productos
+    }catch(error){
+        console.log(error)
+        res.status(404).json({
+            mensaje: "Error. No se pudo obtener la lista de productos"
         })
     }
 }
--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
-import { controladorPrueba, crearProducto } from "../controllers/productos.controllers";
+import { controladorPrueba, crearProducto, obtenerListaProductos } from "../controllers/productos.controllers";
 
 const router = Router();
 
 router.route("/prueba").get(controladorPrueba)
-router.route("/productos").post(crearProducto)
+router.route("/productos").post(crearProducto).get(obtenerListaProductos)
 
 export default router;
