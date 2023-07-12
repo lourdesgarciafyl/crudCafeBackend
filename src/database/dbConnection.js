c603d9f49830c51e1502378776057d54f1289560
LourdesGarcia <lourdesgarciafyl@gmail.com>
2023-07-12 17:20:09 -0300

    conectamos a la base de datos mongo atlas y modificamos db conection

--- src/database/dbConnection.js
+++ src/database/dbConnection.js
@@ -1,7 +1,8 @@
 // me conecto a la base de datos
 import mongoose, { mongo } from "mongoose";
+import "dotenv/config"
 
-const uri = "mongodb://127.0.0.1:27017/crudcafe" // la ultima parte de crudcafe, eso lo elijo yo
+const uri = process.env.DATABASE_URI || "mongodb://127.0.0.1:27017/crudcafe" // la ultima parte de crudcafe, eso lo elijo yo
 mongoose.connect(uri)
 
 const datosConexion = mongoose.connection;
